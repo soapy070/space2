323be328224b66b4b99679152943e4668dbc697c
soapy070 <118913987+soapy070@users.noreply.github.com>
2023-02-21 21:46:23 +0000

    testing button api functions

---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+Author: Iain Watson
+Web page for space
+*/
+
+const burger = document.querySelector("nav svg");
+
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
+    gsap.set("body", { overflow: "auto" });
+    gsap.set("body", { overflowX: "hidden" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: 0 },
+      { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
+    );
+    gsap.set("body", { overflow: "hidden" });
+  }
+  burger.classList.toggle("active");
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
+});
+
+/*const imgDiv = document.getElementById("imageDiv");
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=3`)
+.then(response => response.json())
+.then(data => {
+  imgDiv.innerHTML = data.map(item => `<div>
+ <h3 class="title">${item.title} <span class="date">${item.date}</span></h3>
+ <img src="${item.url}" />
+ <p>${item.explanation}</p></div>`).join('')
+
+})*/
